3adc5b35afe0cfa169077a886ccd86ea0f628bba
NicolasParvedyLaPlateforme <nicolas.parvedy@laplateforme.io>
2026-06-07 22:47:19 +0200

    test

--- app/lib/mail.ts
+++ app/lib/mail.ts
@@ -2,7 +2,7 @@ import nodemailer from 'nodemailer';
 
 const getDomain = () => {
   if (process.env.NEXT_PUBLIC_APP_URL) return process.env.NEXT_PUBLIC_APP_URL;
-  if (process.env.VERCEL_PROJECT_PRODUCTION_URL && process.env.VERCEL_ENV === "production") return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
+  if (process.env.VERCEL_PROJECT_PRODUCTION_URL) return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
   if (process.env.NEXTAUTH_URL) return process.env.NEXTAUTH_URL;
   return "http://localhost:3000";
