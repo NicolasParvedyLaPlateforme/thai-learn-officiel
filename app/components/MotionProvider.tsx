01b59f2a5b372cd939e220a096ae9e56ccd2340b
NicolasParvedyLaPlateforme <nicolas.parvedy@laplateforme.io>
2026-06-08 21:49:59 +0200

    scroll down fix

--- app/components/MotionProvider.tsx
+++ app/components/MotionProvider.tsx
@@ -2,7 +2,7 @@
 
 import { LazyMotion } from "motion/react";
 
-const loadFeatures = () => import("motion/react").then(res => res.domAnimation);
+const loadFeatures = () => import("motion/react").then(res => res.domMax);
 
 export default function MotionProvider({ children }: { children: React.ReactNode }) {
   return (
