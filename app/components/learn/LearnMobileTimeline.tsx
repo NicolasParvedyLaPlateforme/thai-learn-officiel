74678e474aa3fda3926903af1856e9024388335d
NicolasParvedyLaPlateforme <nicolas.parvedy@laplateforme.io>
2026-06-19 15:14:54 +0200

    fix last

--- app/components/learn/LearnMobileTimeline.tsx
+++ app/components/learn/LearnMobileTimeline.tsx
@@ -37,6 +37,7 @@ export default function LearnMobileTimeline({
   mounted,
   handleUnitSelect,
   setIsUnitsModalOpen,
+  setIsQuestsModalOpen,
   setSelectedLesson,
   setModalLevel,
   setLockedReviewModalOpen,
