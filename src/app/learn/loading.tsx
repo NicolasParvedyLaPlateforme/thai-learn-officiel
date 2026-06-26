cc66d78bd02a04c55a51c7869cc7587d66f72041
NicolasParvedyLaPlateforme <nicolas.parvedy@laplateforme.io>
2026-06-26 13:56:07 +0200

    Update loading.tsx

--- src/app/learn/loading.tsx
+++ src/app/learn/loading.tsx
@@ -1,12 +1,5 @@
 import PageLoadingSkeleton from '@/components/layout/PageLoadingSkeleton';
 
 export default function Loading() {
-  return (
-    <PageLoadingSkeleton
-      heroHeight="h-[280px]"
-      progressSegments={4}
-      timelineLineLeftDesktop="left-[3.25rem] md:left-[4.25rem]"
-      timelineLineDesktopWidth="w-2.5"
-    />
-  );
+  return <PageLoadingSkeleton />;
 }
